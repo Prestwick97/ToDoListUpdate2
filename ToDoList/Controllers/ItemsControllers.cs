9348ab000fdb2e1ff86165ae7d25bd06f00ddb45
Student <example@example.com>
2020-03-10 15:15:15 -0700

    continue code-along and add RESTful routing.

--- ToDoList/Controllers/ItemsControllers.cs
+++ ToDoList/Controllers/ItemsControllers.cs
@@ -4,25 +4,36 @@ using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
-  public class ItemsController : Controller 
-  {
-    [HttpGet("/items")]
-    public ActionResult Index()
+    public class ItemsController : Controller
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
+        [HttpGet("/items")]
+        public ActionResult Index()
+        {
+            List<Item> allItems = Item.GetAll();
+            return View(allItems);
+        }
+        [HttpGet("/items/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+        [HttpPost("/items")]
+        public ActionResult Create(string description)
+        {
+            Item myItem = new Item(description);
+            return RedirectToAction("Index");
+        }
+        [HttpPost("/items/delete")]
+        public ActionResult DeleteAll()
+        {
+            Item.ClearAll();
+            return View();
+        }
+        [HttpGet("/items/{id}")]
+        public ActionResult Show(int id)
+        {
+            Item foundItem = Item.Find(id);
+            return View(foundItem);
+        }
     }
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
-      return View();
-    }
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-    }
-
-  }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,27 +2,34 @@ using System;
 using System.Collections.Generic;
 namespace ToDoList.Models
 {
-  public class Item
-  {
-    public string Description { get; set; }
-    private static List<Item> _instances = new List<Item> {};
+    public class Item
+    {
+        public string Description { get; set; }
+        public int Id { get; }
+        private static List<Item> _instances = new List<Item> { };
 
-    public Item(string description)
-    {
-      Description = description;
-      _instances.Add(this);
+        public Item(string description)
+        {
+            Description = description;
+            _instances.Add(this);
+            Id = _instances.Count;
+        }
+        public static List<Item> GetAll()
+        {
+            if (_instances.Count == 0)
+            {
+                Console.WriteLine("No Items in List");
+            }
+            return _instances;
+        }
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
+        public static Item Find(int searchId)
+        {
+            return _instances[searchId - 1];
+        }
+
     }
-    public static List<Item> GetAll()
-    {
-      if(_instances.Count == 0)
-      {
-        Console.WriteLine("No Items in List");
-      }
-      return _instances;
-    }
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-  }
 }
